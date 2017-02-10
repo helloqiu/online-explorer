fc46ed5dc0904681720931ac2ef9e551381bdfd5
Yuyang Zhang <695441493@qq.com>
2017-02-10 18:09:00 +0800

    remove big font file and embed icon in need

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -9,7 +9,5 @@ html, body {
   height: 100%;
   width: 100%;
 }
-$fa-font-path: "../node_modules/font-awesome/fonts";
 @import "../node_modules/bulma/bulma.sass";
-@import "../node_modules/font-awesome/scss/font-awesome.scss";
 </style>
