e59237d7561745ec24eea171d446d6d59111394d
Yuyang Zhang <695441493@qq.com>
2017-02-10 14:56:22 +0800

    package all static file

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -4,9 +4,12 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 html, body {
   height: 100%;
   width: 100%;
 }
+$fa-font-path: "../node_modules/font-awesome/fonts";
+@import "../node_modules/bulma/bulma.sass";
+@import "../node_modules/font-awesome/scss/font-awesome.scss";
 </style>
